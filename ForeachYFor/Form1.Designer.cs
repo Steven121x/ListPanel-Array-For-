c71760ee8263140d3927b5a7920ed26cccc07bd1
Steven121x <Steven121x@gmail.com>
2022-08-24 06:47:38 -0400

    hola

---
+++ ForeachYFor/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace ForeachYFor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Added = new System.Windows.Forms.TextBox();
+            this.Play = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ver ArrayFor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(21, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(498, 251);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(366, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 395);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 57);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Added
+            // 
+            this.Added.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Added.Location = new System.Drawing.Point(21, 280);
+            this.Added.Multiline = true;
+            this.Added.Name = "Added";
+            this.Added.Size = new System.Drawing.Size(498, 97);
+            this.Added.TabIndex = 5;
+            this.Added.Text = "Inserte texto luego presiona Add";
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(21, 467);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(153, 57);
+            this.Play.TabIndex = 6;
+            this.Play.Text = "Leer textbox";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(189, 467);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Volumen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(206, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Speed";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(263, 458);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(256, 45);
+            this.trackBar1.TabIndex = 9;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(263, 496);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(256, 45);
+            this.trackBar2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(540, 533);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Added);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox Added;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ ForeachYFor/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace ForeachYFor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Miembros()
+        {
+            Miembros[] MiembrosS = new Miembros[5];
+            MiembrosS[0] = new Miembros("Juan", 40);
+            MiembrosS[1] = new Miembros("Pedro", 18);
+            MiembrosS[2] = new Miembros("Edu", 20);
+            MiembrosS[3] = new Miembros("Eloy", 23);
+            MiembrosS[4] = new Miembros("Evan", 25);
+            //NOTA:
+            //Hacer limite de ver Array
+            string VariablePendiente;
+            
+            for (int i = 0; i < MiembrosS.Length; i++)
+            {
+                if (listBox1.Items.ToString() != MiembrosS[i].getMembers())
+                {
+                    listBox1.Items.Add(MiembrosS[i].getMembers());
+                }
+                else
+                {
+
+                    MessageBox.Show("Ya hay texto en la listbox");
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Miembros();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(Added.Text.ToString());
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer PL = new SpeechSynthesizer();
+            PL.Volume = trackBar1.Value;
+            PL.Rate = trackBar2.Value;
+            PL.Speak(Added.Text.ToString());                                                                                                                              
+        }
+    }
+
+
+    class Miembros
+    {
+        public Miembros(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        private string nombre;
+        private int edad;
+
+        public string getMembers()
+        {
+            return "Nombre: " + nombre + " | Edad: " + edad;
+        }
+
+    }
+}
